79573efbb5604dafe006d0318b3f624c84c4d475
Amy Gori <amydgori@gmail.com>
2023-02-15 20:23:45 -0500

    Add consumer, channel layer, and redis

--- lechat/asgi.py
+++ lechat/asgi.py
@@ -10,8 +10,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+import chatness.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lechat.settings")
 
-application = ProtocolTypeRouter({"http": get_asgi_application()})
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chatness.routing.websocket_urlpatterns))
+        ),
+    }
+)
