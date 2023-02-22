cf7840a62d755849cf175bbcf5bbd3b260c78b1d
Amy Gori <amydgori@gmail.com>
2023-02-21 20:46:54 -0500

    Set static root for deployment

--- lechat/settings.py
+++ lechat/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.messages",
     'django_extensions',
     "api",
 ]
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
