e7f4cde3e85a2e186f9a0c89b3e2a005484f0afd
Amy Gori <amydgori@gmail.com>
2023-03-05 11:42:13 -0500

    Initialize the application before importing middleware

--- lechat/asgi.py
+++ lechat/asgi.py
@@ -13,13 +13,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 import chatness.routing
-from lechat.token_auth_middleware import TokenAuthMiddleware
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lechat.settings")
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from lechat.token_auth_middleware import TokenAuthMiddleware
 
 application = ProtocolTypeRouter(
     {
